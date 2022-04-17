678eea1a13ed1e855e6d211db9572b71da9d9566
Your Name <sarifulislaml8280@gmail.com>
2022-04-17 15:28:27 +0600

    about done

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,11 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+
+import React, { useEffect, useRef, useState } from 'react';
+import { Button, Form, Spinner } from 'react-bootstrap';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import GoogleLogin from '../components/GoogleLogin/GoogleLogin';
 import auth from '../firebase.init';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
+    const emailRef = useRef('')
+    const navigate = useNavigate()
+    const location = useLocation();
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -16,7 +22,9 @@ const Login = () => {
         error,
     ] = useSignInWithEmailAndPassword(auth);
 
-    console.log(user);
+    // console.log(user);
+    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+
 
     const handleEmail = (e) => {
 
@@ -32,12 +40,7 @@ const Login = () => {
         e.preventDefault();
         signInWithEmailAndPassword(email, password)
     }
-    const navigate = useNavigate()
-    // if (user) {
-    //     navigate('/banner')
-    // }
-    // const navigate = useNavigate();
-    const location = useLocation();
+
     const from = location.state?.from?.pathname || "/";
 
     useEffect(() => {
@@ -51,9 +54,28 @@ const Login = () => {
     if (error) {
         errorMsg = <p className='text-danger'>{error?.message}</p>
     }
+
+    if (loading || sending) {
+        return <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+        </Spinner>
+    }
+
     const navigateToRegister = () => {
         navigate('/register')
     }
+
+    const resetPassword = async () => {
+        // const email = emailRef.current.value;
+        if (email) {
+            await sendPasswordResetEmail(email);
+            toast('Email send')
+        }
+        else {
+            toast('please enter your email address');
+        }
+    }
+
     return (
         <div className='container w-50 mt-5'>
             <h3 className='text-primary'>Please Login</h3>
@@ -71,6 +93,8 @@ const Login = () => {
                     <Form.Control onBlur={handlePassword} type="password" placeholder="Password" required />
                 </Form.Group>
                 {errorMsg}
+                <p>Forget Password? <button className='btn btn-link text-primary pe-auto text-decoration-none' onClick={resetPassword}>Reset Password</button> </p>
+
                 <p>Dont't have any account? <Link to="/register" className='text-primary pe-auto text-decoration-none' onClick={navigateToRegister}>Please Register</Link> </p>
                 <Button variant="primary" type="submit">
                     Login
@@ -78,6 +102,8 @@ const Login = () => {
 
             </Form>
             <GoogleLogin></GoogleLogin>
+            <ToastContainer />
+
         </div>
     );
 };
